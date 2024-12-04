b58fddcabe9dd0f14d2d45ebcf587b2a5ddbfd29
Jackeline Ferreira <conecta.online9891@gmail.com>
2024-12-04 20:00:52 -0300

    Alterações realizadas nos arquivos footer-header-inicio-livrosdoados-querodoar

--- src/pages/Livros doados/LivrosDoados.jsx
+++ src/pages/Livros doados/LivrosDoados.jsx
@@ -3,19 +3,19 @@ import s from './LivrosDoados.module.scss'
 
 export default function LivrosDoados() {
   return (
-    <main>
-        <section className={s.LivrosDoacao}>
-            <h2>Livros Doados</h2>
-            <div className={s.LivrosDoados}>
-            <section>
-                <img src={livro1} alt="Imagem livro doado" />
-                <p>O protagonista</p>
-                <p>Susanne Andrade</p>
-                <p>Ficção</p>
-            </section>
-            </div>
-        </section>
-    </main>
-    );
-  }
+    <section className={s.livrosDoados}>
+      <h2>Livros Doados</h2>
+      <section>
+        <img src={livro1} alt="Capa do livro o protagonista" />
+        <div className={s.livro}>
+          <h3>O Protagonista</h3>
+          <p>Susane Andrade</p>
+          <p>Ficção</p>
+        </div>
+      </section>
+
+    </section>
+
+  );
+}
   
